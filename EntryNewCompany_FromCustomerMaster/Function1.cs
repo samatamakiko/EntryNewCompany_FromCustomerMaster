0bbf88bdd26dd5b1eefcf2b93200817caabed91b
makiko.samata <makiko.samata@keycrew.co.jp>
2023-07-18 18:11:17 +0900

    必須項目がNULLでないか確認途中

--- EntryNewCompany_FromCustomerMaster/Function1.cs
+++ EntryNewCompany_FromCustomerMaster/Function1.cs
@@ -11,6 +11,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Collections.Generic;
 using static System.Net.Mime.MediaTypeNames;
+using Dapper;
 
 namespace EntryNewCompany_FromCustomerMaster
 {
@@ -25,48 +26,54 @@ namespace EntryNewCompany_FromCustomerMaster
             using var fileStream = new FileStream("./phonic-monolith-392109-d6f2255c2bc6.json", FileMode.Open, FileAccess.Read);
             var googleCredential = GoogleCredential.FromStream(fileStream).CreateScoped(SheetsService.Scope.Spreadsheets);
             var sheetsService = new SheetsService(new BaseClientService.Initializer() { HttpClientInitializer = googleCredential });
-
             var spreadsheetId = "1MjYPr8x8hzd9t-1nR6HgcKicK0gTfzsSq2Q0zzpgPSg";
             var range = "å⁄ãqÉ}ÉXÉ^!A2:AL";
-
             var request = sheetsService.Spreadsheets.Values.Get(spreadsheetId, range);
             var response = request.Execute();
             var values = response.Values.ToList();
 
-           
             List<Company> CompanyList = new List<Company>();
-            foreach (var value in values)
+
+            using var connection = new SqliteConnection(values);
+            using (var command = connection.CreateCommand())
             {
-                if (value.Count() == 38)
+                connection.Open();
+                var sql = "[dbo].[SP_CompanyInitData]";
+                var result = connection.Queary<Company>(sql);
+
+                foreach (var value in values)
                 {
-                    CompanyList.Add(new Company()
+                    //value.CountÇÃêîÇ™38å¬Ç≈Ç†ÇÍÇŒèoóÕ
+                    if (value.Count() == 38)
                     {
-                        CompanyCode = (string)value[5] ?? "",
-                        CompanyName = (string)value[0] ?? "",
-                        CompanyKana = (string)value[15] ?? "",
-                        PostCode = (string)value[17] ?? "",
-                        CompanyAbbreviation = (string)value[37] ?? "",
-                        Prefectures = (string)value[18] ?? "",
-                        ComponyAddress1 = (string)value[19] ?? "",
-                        ComponyAddress2 = (string)value[20] ?? "",
-                        TelephoneNumber = (string)value[24] ?? "",
-                        CompanyEmail = (string)value[25] ?? "",
-                        SlackEmail = (string)value[29] ?? "",
-                        DockName = (string)value[2] ?? "",
-                    });
-                }
-                else
-                {
-                    Console.WriteLine("");
+                        CompanyList.Add(new Company()
+                        {
+                            CompanyCode = (string)value[5] ?? "",
+                            CompanyName = (string)value[0] ?? "",
+                            Furigana = (string)value[15] ?? "",
+                            ZipCode = (string)value[17] ?? "",
+                            CompanyNameS = (string)value[37] ?? "",
+                            PrefName = (string)value[18] ?? "",
+                            Address1 = (string)value[19] ?? "",
+                            Address2 = (string)value[20] ?? "",
+                            Phone = (string)value[24] ?? "",
+                            Email = (string)value[25] ?? "",
+                            SlackEmail = (string)value[29] ?? "",
+                            DockName = (string)value[2] ?? "",
+                        });
+                    }
+                    else
+                    {
+                        Console.WriteLine("");
+                    }
                 }
             }
-
-         
+                
             foreach (Company c in CompanyList)
             {
-                Console.WriteLine(c.CompanyCode +","+ c.CompanyName +","+ c.CompanyKana +","+ c.PostCode +","+
-                    c.CompanyAbbreviation +","+c.Prefectures +","+ c.ComponyAddress1 +","+ c.ComponyAddress2 +","+
-                    c.TelephoneNumber +","+c.CompanyEmail +","+ c.SlackEmail+","+ c.DockName);
+                Console.WriteLine(c.CompanyCode +","+ c.CompanyName +","+ c.Furigana +","+ c.ZipCode +","+
+                    c.CompanyNameS +","+c.PrefName +","+ c.Address1 +","+ c.Address2 +","+
+                    c.Phone +","+c.Email +","+ c.SlackEmail+","+ c.DockName);
                 
                
             }
@@ -79,14 +86,14 @@ namespace EntryNewCompany_FromCustomerMaster
     {
         public string CompanyCode { get; set; }
         public string CompanyName { get; set; } 
-        public string CompanyKana { get; set; }
-        public string CompanyAbbreviation { get; set; }//ó™èÃ
-        public string PostCode { get; set; }//óXï÷î‘çÜ
-        public string Prefectures { get; set; }//ìsìπï{åß
-        public string ComponyAddress1 { get; set; }
-        public string ComponyAddress2 { get; set; }
-        public string TelephoneNumber { get; set; }
-        public string CompanyEmail { get; set; }
+        public string Furigana { get; set; }
+        public string CompanyNameS { get; set; }//ó™èÃ
+        public string ZipCode { get; set; }//óXï÷î‘çÜ
+        public string PrefName { get; set; }//ìsìπï{åß
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
         public string SlackEmail { get; set; }
         public string DockName { get; set; }
 
