84fdb1723b7497bb599de97b371386ba9922d066
makiko.samata <makiko.samata@keycrew.co.jp>
2023-07-19 11:04:52 +0900

    保存

--- EntryNewCompany_FromCustomerMaster/Function1.cs
+++ EntryNewCompany_FromCustomerMaster/Function1.cs
@@ -12,6 +12,8 @@ using System.Linq;
 using System.Collections.Generic;
 using static System.Net.Mime.MediaTypeNames;
 using Dapper;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
 
 namespace EntryNewCompany_FromCustomerMaster
 {
@@ -27,25 +29,16 @@ namespace EntryNewCompany_FromCustomerMaster
             var googleCredential = GoogleCredential.FromStream(fileStream).CreateScoped(SheetsService.Scope.Spreadsheets);
             var sheetsService = new SheetsService(new BaseClientService.Initializer() { HttpClientInitializer = googleCredential });
             var spreadsheetId = "1MjYPr8x8hzd9t-1nR6HgcKicK0gTfzsSq2Q0zzpgPSg";
-            var range = "顧客マスタ!A2:AL";
+            var range = "顧客マスタ!A431:AL432";
             var request = sheetsService.Spreadsheets.Values.Get(spreadsheetId, range);
             var response = request.Execute();
             var values = response.Values.ToList();
 
             List<Company> CompanyList = new List<Company>();
-
-            using var connection = new SqliteConnection(values);
-            using (var command = connection.CreateCommand())
+            foreach (var value in values)
             {
-                connection.Open();
-                var sql = "[dbo].[SP_CompanyInitData]";
-                var result = connection.Queary<Company>(sql);
-
-                foreach (var value in values)
+                if (value.Count() == 38)
                 {
-                    //value.Countの数が38個であれば出力
-                    if (value.Count() == 38)
-                    {
                         CompanyList.Add(new Company()
                         {
                             CompanyCode = (string)value[5] ?? "",
@@ -61,24 +54,74 @@ namespace EntryNewCompany_FromCustomerMaster
                             SlackEmail = (string)value[29] ?? "",
                             DockName = (string)value[2] ?? "",
                         });
+                 }
+
+            }
+
+            using (var connection = new SqlConnection(CompanyList))
+            {
+                var result = connection.Query<Company>(
+                    "MyStoredProcedure",
+                    commandType: CommandType.StoredProcedure
+                ).ToList();
+
+
+                foreach (Company c in CompanyList)
+                {
+                    //杉戸→1100、八潮→1000に倉庫コード変換
+                    if (c.DockName == "Sugito")
+                    {
+                        c.DockName = c.DockName.Replace("Sugito", "1100");
+                    }
+                    else if (c.DockName == "Yashio")
+                    {
+                        c.DockName = c.DockName.Replace("Yashio", "1000");
+                    }
+
+                    //必須項目に値が入っていなかったらbreakする
+                    if (c.CompanyCode == "")
+                    {
+                        break;
+                    }
+                    else if (c.CompanyName == "")
+                    {
+                        break;
+                    }
+                    else if (c.Furigana == "")
+                    {
+                        break;
+                    }
+                    else if (c.ZipCode == "")
+                    {
+                        break;
+                    }
+                    else if (c.CompanyNameS == "")
+                    {
+                        break;
+                    }
+                    else if (c.PrefName == "")
+                    {
+                        break;
+                    }
+                    else if (c.Address1 == "")
+                    {
+                        break;
+                    }
+                    else if (c.DockName == "")
+                    {
+                        break;
                     }
                     else
                     {
-                        Console.WriteLine("");
+                        Console.WriteLine(c.CompanyCode + "," + c.CompanyName + "," + c.Furigana + "," + c.ZipCode + "," +
+                        c.CompanyNameS + "," + c.PrefName + "," + c.Address1 + "," + c.Address2 + "," +
+                        c.Phone + "," + c.Email + "," + c.SlackEmail + "," + c.DockName);
                     }
+
+
+
                 }
             }
-                
-            foreach (Company c in CompanyList)
-            {
-                Console.WriteLine(c.CompanyCode +","+ c.CompanyName +","+ c.Furigana +","+ c.ZipCode +","+
-                    c.CompanyNameS +","+c.PrefName +","+ c.Address1 +","+ c.Address2 +","+
-                    c.Phone +","+c.Email +","+ c.SlackEmail+","+ c.DockName);
-                
-               
-            }
-
-           
         }
     }
 
