083e46fdc45b240669c8c099cf8f9092170a4b1c
makiko.samata <makiko.samata@keycrew.co.jp>
2023-08-01 15:21:40 +0900

    hozon

--- EntryNewCompany_FromCustomerMaster/Function1.cs
+++ EntryNewCompany_FromCustomerMaster/Function1.cs
@@ -21,6 +21,10 @@ using Newtonsoft.Json;
 using System.ComponentModel;
 using Google.Apis.Requests;
 using System.Collections;
+using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+
 
 namespace EntryNewCompany_FromCustomerMaster
 {
@@ -54,37 +58,34 @@ namespace EntryNewCompany_FromCustomerMaster
                 var aaa = "SELECT CompanyCode FROM TS_CompanyInfo";
                 var sss = connection.Query<string>(aaa);
                 CompanyList2 = sss.Select(s => new Company2()
-                    {
-                        CompanyCode = s
-                    }).ToList();
+                {
+                    CompanyCode = s
+                }).ToList();
             }
 
 
-                List<Company> CompanyList = new List<Company>();
-                foreach (var value in values)
+            List<Company> CompanyList = new List<Company>();
+            foreach (var value in values)
+            {
+                if (value.Count() == 38)
                 {
-                    if (value.Count() == 38)
+                    CompanyList.Add(new Company()
                     {
-                        CompanyList.Add(new Company()
-                        {
-                            CompanyCode = (string)value[5] ?? "",
-                            CompanyName = (string)value[0] ?? "",
-                            Furigana = (string)value[15] ?? "",
-                            ZipCode = (string)value[17] ?? "",
-                            CompanyNameS = (string)value[37] ?? "",
-                            PrefName = (string)value[18] ?? "",
-                            Address1 = (string)value[19] ?? "",
-                            Address2 = (string)value[20] ?? "",
-                            Phone = (string)value[24] ?? "",
-                            Email = (string)value[25] ?? "",
-                            SlackEmail = (string)value[29] ?? "",
-                            DockName = (string)value[2] ?? "",
-                        });
-                    }
+                        CompanyCode = (string)value[5] ?? "",
+                        CompanyName = (string)value[0] ?? "",
+                        Furigana = (string)value[15] ?? "",
+                        ZipCode = (string)value[17] ?? "",
+                        CompanyNameS = (string)value[37] ?? "",
+                        PrefName = (string)value[18] ?? "",
+                        Address1 = (string)value[19] ?? "",
+                        Address2 = (string)value[20] ?? "",
+                        Phone = (string)value[24] ?? "",
+                        Email = (string)value[25] ?? "",
+                        SlackEmail = (string)value[29] ?? "",
+                        DockName = (string)value[2] ?? "",
+                    });
                 }
-            
-
-
+            }
 
 
             foreach (Company c in CompanyList)
@@ -143,60 +144,62 @@ namespace EntryNewCompany_FromCustomerMaster
                      c.Phone + "," + c.Email + "," + c.SlackEmail + "," + c.DockName);*/
                 }
 
-                // check company code
-                if (CompanyList2.Any(d => d.CompanyCode == c.CompanyCode))
+                
+
+               var result = CompanyList2.Any(d => d.CompanyCode.Contains(c.CompanyCode));
+               
+                if (result == true)
                 {
-                    break;
-                }
-
-
-                var connectionString = @"Data Source=127.0.0.1;Initial Catalog=BAW; Integrated Security=SSPI;";
-                using (var connection = new SqlConnection(connectionString))
-                {
-                    var parameters = new DynamicParameters();
-                    parameters.Add("@CompanyCode", c.CompanyCode);
-                    parameters.Add("@Cmd", "Add");
-                    parameters.Add("@CompanyName", c.CompanyName);
-                    parameters.Add("@Furigana", c.Furigana);
-                    parameters.Add("@CompanyNameS", c.CompanyNameS);
-                    parameters.Add("@PrefName", c.PrefName);
-                    parameters.Add("@ZipCode", c.ZipCode);
-                    parameters.Add("@Address1", c.Address1);
-                    parameters.Add("@Address2", c.Address2);
-                    parameters.Add("@Phone", c.Phone);
-                    parameters.Add("@Email", c.Email);
-                    parameters.Add("@SlackEmail", c.SlackEmail);
-                    parameters.Add("@SiteCode", c.DockName);
-                    parameters.Add("@return_value", dbType: DbType.Int32, direction: ParameterDirection.ReturnValue);
-
+                    CompanyCheck = "";
                     
+                } else if (result == false)
+                {
 
-                    var result = connection.Query<Company>(
-                       "[dbo].[SP_CompanyInitData]",
-                       parameters, commandType: CommandType.StoredProcedure);
-
-                    var output = parameters.Get<int>("@return_value");
-
-                    Console.WriteLine($"{output}");
-
-
-
-
-
-                    if (output == 0)
+                    var connectionString = @"Data Source=127.0.0.1;Initial Catalog=BAW; Integrated Security=SSPI;";
+                    using (var connection = new SqlConnection(connectionString))
                     {
-                        CompanyCheck = CompanyCheck + "\r" + $"ÅöÅöÅö{c.CompanyName}Ç™BAWÇ÷ìoò^Ç≥ÇÍÇ‹ÇµÇΩÅöÅöÅö";
-                    }
-                    else
-                    {
-                        CompanyCheck = "ìoò^Ç≈Ç´ÇÈ";
-                    }
+                        var parameters = new DynamicParameters();
+                        parameters.Add("@CompanyCode", c.CompanyCode);
+                        parameters.Add("@Cmd", "Add");
+                        parameters.Add("@CompanyName", c.CompanyName);
+                        parameters.Add("@Furigana", c.Furigana);
+                        parameters.Add("@CompanyNameS", c.CompanyNameS);
+                        parameters.Add("@PrefName", c.PrefName);
+                        parameters.Add("@ZipCode", c.ZipCode);
+                        parameters.Add("@Address1", c.Address1);
+                        parameters.Add("@Address2", c.Address2);
+                        parameters.Add("@Phone", c.Phone);
+                        parameters.Add("@Email", c.Email);
+                        parameters.Add("@SlackEmail", c.SlackEmail);
+                        parameters.Add("@SiteCode", c.DockName);
+                        parameters.Add("@return_value", dbType: DbType.Int32, direction: ParameterDirection.ReturnValue);
 
+
+
+                        var result2 = connection.Query<Company>(
+                           "[dbo].[SP_CompanyInitData]",
+                           parameters, commandType: CommandType.StoredProcedure);
+
+                        var output = parameters.Get<int>("@return_value");
+
+                        Console.WriteLine(c.CompanyCode);
+                        Console.WriteLine($"{output}");
+
+
+                        /*string stringOutput = output.ToString();
+                        Console.WriteLine(stringOutput.GetType());
+
+*/
+                        if (output == 0)
+                        {
+                            CompanyCheck = CompanyCheck + "\r" + $"ÅöÅöÅö{c.CompanyName}Ç™BAWÇ÷ìoò^Ç≥ÇÍÇ‹ÇµÇΩÅöÅöÅö";
+                        }
+                    }
+                    
                 }
             }
             return new OkObjectResult(CompanyCheck);
         }
-
     }
 
     public class Company
@@ -218,7 +221,7 @@ namespace EntryNewCompany_FromCustomerMaster
     public class Company2
     {
         public string CompanyCode { get; set; }
-      
+
     }
 
 }
